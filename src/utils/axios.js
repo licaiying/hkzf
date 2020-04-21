dd121732d7370cbd3cfe1173f5fd65a30b97f48a
licaiying <1194449590@qq.com>
2020-04-21 17:59:57 +0800

    axios拦截器->全局请求的开始和关闭动画

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,6 +4,9 @@
 
 import axios from 'axios'
 
+// 导入 加载效果 的Toast组件
+import { Toast } from 'antd-mobile'
+ 
 // 后台的基础路径
 const BASE_URL = 'http://api-haoke-dev.itheima.net'
 
@@ -16,6 +19,10 @@ const axiosAPI = axios.create({
 // 2.1 请求拦截器(请求之前调用)
 axiosAPI.interceptors.request.use(function(config){
     // console.log('开始请求了',config)
+
+    // 数据请求时的加载效果
+    Toast.loading('加载中...',0) // 0：表示只要不手动调用 hide，就会一直加载
+
     return config
 },function(error){
     return Promise.reject(error)
@@ -25,6 +32,9 @@ axiosAPI.interceptors.request.use(function(config){
 // 2.2 响应拦截器(请求成功调用)
 axiosAPI.interceptors.response.use(function(response){
     // console.log('请求成功了',response)
+
+    // 数据请求成功后，关闭 加载中 的动画效果
+    Toast.hide()
 
     // 设计一个新的简化的数据结构，然后返回
     let newRes = {
