6e38760f2ed7738b2b9ace8e290697705120a080
licaiying <1194449590@qq.com>
2020-04-21 14:04:18 +0800

    首页-轮播图-请求数据并渲染

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,23 +1,46 @@
 import React, { Component } from 'react';
 
+// 导入 axios
+import axios from 'axios';
+
 // 导入 走马灯(轮播图)组件
 import { Carousel } from 'antd-mobile'
 
+
 class Index extends Component {
+
     state = {
         // 轮播图的数据
-        swiper: ['1', '2', '3'],
+        swiper: [],
         // 设置轮播图的默认高度
         imgHeight: 176,
       }
+
+
       componentDidMount() {
-        // simulate img loading
-        setTimeout(() => {
-          this.setState({
-            swiper: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
-          });
-        }, 100);
+        // 调用函数，获取轮播图数据，渲染页面
+        this.getSwiper()
       }
+
+
+      // 获取轮播图的数据
+      getSwiper =  async () => {
+        const res = await axios.get('http://api-haoke-dev.itheima.net/home/swiper')
+        // console.log(res)
+        if (res.status === 200) {
+            // 处理图片的路径
+            // res.data.body.forEach((item)=>{
+            //     item.imgSrc = `http://api-haoke-dev.itheima.net${item.imgSrc}`
+            // })
+
+            // 修改 swiper 状态数据
+            this.setState({
+                swiper:res.data.body
+            })
+        }
+      }
+
+
       render() {
         return (
           <div className="index">
@@ -27,12 +50,12 @@ class Index extends Component {
             >
               {this.state.swiper.map(val => (
                 <a
-                  key={val}
-                  href="http://www.alipay.com"
+                  key={val.id}
+                  href="http://www.itheima.com"
                   style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
                 >
                   <img
-                    src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
+                    src={`http://api-haoke-dev.itheima.net${val.imgSrc}`}
                     alt=""
                     style={{ width: '100%', verticalAlign: 'top' }}
                     onLoad={() => {
