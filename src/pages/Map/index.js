8ced64a1fd726a1e28ee0923db55242388ac5be1
licaiying <1194449590@qq.com>
2020-04-22 17:47:19 +0800

    首页-百度地图API-介绍-使用

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import './index.scss'
 
 class Map extends Component {
+
+    componentDidMount(){
+        this.initMap()
+    }
+
+    // 初始化地图
+    initMap = () => {
+        // console.log(window)
+        const { BMap } = window
+        // console.log(BMap)
+
+        // 1.创建地图实例
+        const map = new BMap.Map("container");
+        // 2.设置中心点坐标(默认是天安门)
+        let point = new BMap.Point(116.404, 39.915); 
+        // 3.地图初始化，同时设置地图展示级别(设置地图的位置和缩放级别)
+        map.centerAndZoom(point, 15); 
+    }
+
+
     render() {
         return (
-            <div>
-                Map
+            <div className="mapBox">
+               <div id="container"></div> 
             </div>
         );
     }
