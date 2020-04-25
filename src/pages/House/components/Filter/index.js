d526c41a871be2e1d803233c146287efb079c3b6
licaiying <1194449590@qq.com>
2020-04-25 20:50:05 +0800

    列表找房-FilterPicker-显示

--- src/pages/House/components/Filter/index.js
+++ src/pages/House/components/Filter/index.js
@@ -26,7 +26,9 @@ export default class Filter extends Component {
   // 定义状态数据
   state = {
     // 高亮的数据状态
-    titleSelectedStatus
+    titleSelectedStatus,
+    // 是否显示Picker组件
+    openType:''  // 默认为空，不显示
   }
 
   // 父组件：提供 修改数据高亮状态的方法，给子组件使用
@@ -37,21 +39,33 @@ export default class Filter extends Component {
 
     // 拷贝一份新的高亮状态数据
     // [type]:true:ES6写法，可给变量直接赋值
-    // {...titleSelectedStatus,[type]:true} => 当点击的[type]的值，变为true时，覆盖前面的...titleSelectedStatus的对应的值
+    // {...titleSelectedStatus,[type]:true} => 当点击的[type]的值，变为true时，替换前面的...titleSelectedStatus的对应的值
     let newSelected = {...titleSelectedStatus,[type]:true} 
     // console.log(newSelected) // {area: false, mode: false, price: false, more: true}
 
     // 根据点击的数据，显示相应的高亮状态
     this.setState({
-      titleSelectedStatus:newSelected
+      titleSelectedStatus:newSelected,
+      openType:type  // 将点击时的type值，赋值给openType，以显示Picker组件
     })
   }
+
+
+  // 定义方法，判断是否显示前三个过滤器的内容,即Picker组件的显示
+  isShowPicker = () => {
+    const { openType } = this.state
+    // 只有 openType的值，为以下数据时，才显示Picker组件，以展示对应的内容
+    return openType === 'area' || openType === 'mode' || openType === 'price'
+  }
+
 
   render() {
     return (
       <div className={styles.root}>
         {/* 前三个菜单的遮罩层 */}
-        {/* <div className={styles.mask} /> */}
+        {
+          this.isShowPicker() ? <div className={styles.mask} /> : null
+        }
 
         <div className={styles.content}>
           {/* 标题栏 */}
@@ -59,7 +73,9 @@ export default class Filter extends Component {
           <FilterTitle titleSelectedStatus={this.state.titleSelectedStatus} onTitleClick={this.onTitleClick} />
 
           {/* 前三个菜单对应的内容： */}
-          {/* <FilterPicker /> */}
+          {
+            this.isShowPicker() ? <FilterPicker /> : null
+          }
 
           {/* 最后一个菜单对应的内容： */}
           {/* <FilterMore /> */}
