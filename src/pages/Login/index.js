933da50655e34ece7c6d083d1ef94da8bc42ddc2
licaiying <1194449590@qq.com>
2020-04-29 13:30:49 +0800

    使用formik组件重构Login，实现表单数据的双向绑定

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -7,6 +7,8 @@ import { login } from '../../utils/api/User'
 import styles from './index.module.css'
 import { HKZF_TOKEN } from '../../utils/index.js'
 
+import { withFormik } from 'formik';
+
 // 验证规则：
 // const REG_UNAME = /^[a-zA-Z_\d]{5,8}$/
 // const REG_PWD = /^[a-zA-Z_\d]{5,12}$/
@@ -14,21 +16,21 @@ import { HKZF_TOKEN } from '../../utils/index.js'
 class Login extends Component {
 
   // 设置状态数据
-  state = {
-    // 用户名
-    username:'test2',
-    // 密码
-    password:'test2'
-  }
+  // state = {
+  //   // 用户名
+  //   username:'test2',
+  //   // 密码
+  //   password:'test2'
+  // }
 
 
-  // 受控组件(双向绑定) => 一个事件，处理多个表单元素
-  handlerChange = (e) => {
-    // console.log(e.target.name) // username password
-    this.setState({
-      [e.target.name]:e.target.value
-    })
-  }
+  // // 受控组件(双向绑定) => 一个事件，处理多个表单元素
+  // handlerChange = (e) => {
+  //   // console.log(e.target.name) // username password
+  //   this.setState({
+  //     [e.target.name]:e.target.value
+  //   })
+  // }
 
   // 登录
   login = async (e) => {
@@ -52,6 +54,16 @@ class Login extends Component {
   }
 
   render() {
+
+    const {
+      values,
+      touched,
+      errors,
+      handleChange,
+      handleBlur,
+      handleSubmit,
+    } = this.props;
+
     return (
       <div className={styles.root}>
         {/* 顶部导航 */}
@@ -67,8 +79,8 @@ class Login extends Component {
               <input
                 className={styles.input}
                 name="username"
-                value={this.state.username}
-                onChange={this.handlerChange}
+                value={values.username}
+                onChange={handleChange}
                 placeholder="请输入账号"
               />
             </div>
@@ -79,8 +91,8 @@ class Login extends Component {
                 className={styles.input}
                 name="password"
                 type="password"
-                value={this.state.password}
-                onChange={this.handlerChange}
+                value={values.password}
+                onChange={handleChange}
                 placeholder="请输入密码"
               />
             </div>
@@ -103,4 +115,29 @@ class Login extends Component {
   }
 }
 
-export default Login
+// 返回了增强的新组件 => 基础表单处理和校验
+const NewLogin = withFormik({
+  mapPropsToValues: () => ({ username: '111',password:'222' }),
+
+  // Custom sync validation
+  // validate: values => {
+  //   const errors = {};
+
+  //   if (!values.name) {
+  //     errors.name = 'Required';
+  //   }
+
+  //   return errors;
+  // },
+
+  handleSubmit: (values, { setSubmitting }) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 1000);
+  },
+
+  displayName: 'BasicForm',
+})(Login);
+
+export default NewLogin
